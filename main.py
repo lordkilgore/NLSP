37f656015efabf9d6961a27b30f2178d7def37be
phoenix <67361789+lordkilgore@users.noreply.github.com>
2023-11-02 12:14:38 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+class NLSP:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def backsub(U, y):
+        # Initialization
+        n = U.shape[0] - 1
+
+        x = np.array([float(i) for i in range(n + 1)])
+        x[n] = y[n] / U[n][n]
+        
+        for i in range(n):
+            entry_num = n - 1 - i
+
+            # Inner Product between row of U and x, excluding the last row
+            row_sum = 0
+            for k in range(n - entry_num):
+                row_sum += U[entry_num][entry_num + k + 1] * x[entry_num + k + 1]
+            
+            # Backwards Substitution
+            x[entry_num] = 1 / (U[entry_num][entry_num]) * (y[entry_num] - row_sum)
+        
+        return x
+
+    @staticmethod
+    def forwardsub(L, y):
+        # Initialization
+        n = L.shape[0] - 1
+        x = np.array([float(1) for i in range(n + 1)])
+        x[0] = y[0] / L[0][0]
+
+        for i in range(n):
+            entry_num = i + 1
+
+            # Inner Product between row of L and x, excluding the first row
+            row_sum = 0
+            for k in range(entry_num):
+                row_sum += L[entry_num][k] * x[k]
+            
+            # Forward Substitution
+            x[entry_num] = 1 / (L[entry_num][entry_num]) * (y[entry_num] - row_sum)
+        
+        return x
+
+    @staticmethod
+    def gaussian(A, y = None):
+        n = A.shape[0] - 1
+
+        for i in range(n):
+            if i > 0:
+                pass
+
+            e = np.zeros(n+1)
+            tau = np.zeros(n+1)
+            e[i] = 1
+            for k in range(n+1):
+                if k <= i:
+                    pass
+                else:
+                    tau[k] = A[k][i]
+            tau = 1 / A[i][i] * tau
+
+            L = (np.identity(n + 1) - np.outer(tau, e))
+
+            A = np.matmul(L, A)
+            if type(y) != None:
+                y = np.matmul(L, y)
+        
+        if type(y) != None:
+            return A, y
+        else:
+            return A
+    
+    @staticmethod
+    def solve(A, b):
+        if np.linalg.det(A) > 0:
+            U, y = NLSP.gaussian(A, b)
+            x = NLSP.backsub(U, y)
+            return x
+        else:
+            return f"{A} is singular."
